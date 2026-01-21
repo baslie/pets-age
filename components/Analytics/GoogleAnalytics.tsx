f3b61a38e3e1a5769ec5ac4dbafddbc0daa1dcd2
Роман Пуртов <rytrycon@gmail.com>
2026-01-21 12:31:05 +0700

    refactor: use @next/third-parties for Google Analytics
    Replace custom Script implementation with official GoogleAnalytics
    component from @next/third-parties/google. Also hardcode the GA
    measurement ID similar to Yandex Metrika.

--- components/Analytics/GoogleAnalytics.tsx
+++ components/Analytics/GoogleAnalytics.tsx
@@ -1,27 +1,9 @@
 'use client';
 
-import Script from 'next/script';
+import { GoogleAnalytics as GA } from '@next/third-parties/google';
 import { GA_MEASUREMENT_ID } from '@/lib/analytics';
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) return null;
-
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            cookie_flags: 'SameSite=None;Secure'
-          });
-        `}
-      </Script>
-    </>
-  );
+  return <GA gaId={GA_MEASUREMENT_ID} />;
 }
--- lib/analytics.ts
+++ lib/analytics.ts
@@ -9,7 +9,7 @@ declare global {
 }
 
 // Environment variables for analytics IDs
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+export const GA_MEASUREMENT_ID = 'G-CKNLH77CMK';
 export const YM_COUNTER_ID = 106346783;
 
 // Check if analytics is enabled
